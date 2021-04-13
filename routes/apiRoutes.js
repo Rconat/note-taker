af0b9b9129a5588c216d6f2d577ee7d412326362
ryzecous <r3j9c89@gmail.com>
2021-04-12 21:20:07 -0400

    added app.post to apiRoutes.js - used uuid to generate an ID for the added note

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,28 @@ const fs = require('fs')
 const uuid = require('uuid')
 
 module.exports = function (app) {
-    app.get('/api/notes', function (req, res) {
+
+    // app.get
+    app.get('/api/notes', (req, res) => {
         res.send(db)
     })
+
+    // app.post
+    app.post('/api.notes', (req, res) => {
+        const addNote = req.body
+        addNote.id = uuid.v4()
+
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) throw err
+
+            // concat addNote onto newNotes
+            const newNotes = JSON.parse(data).concat(addNote)
+            fs.writeFile('./db/db.json', JSON.stringify(newNotes), (err, data) => {
+                if (err) throw err
+                res.json(newNotes)
+            })
+        })
+    })
+
+    // app.delete
 }
